9f39f3feef0606927b9b0c461d088f309d97da1c
agservizi <ag.servizi16@gmail.com>
2025-05-23 09:27:11 +0200

    Aggiunta configurazione per la visualizzazione degli errori nel file di login

--- login.php
+++ login.php
@@ -3,6 +3,10 @@ session_start();
 require_once __DIR__ . '/src/controllers/AuthController.php';
 use CoreSuite\Controllers\AuthController;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $controller = new AuthController();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $controller->login();
